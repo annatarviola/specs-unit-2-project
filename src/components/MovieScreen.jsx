87e50336d2902bd2aa6c5b1e79b351171789e34a
Lily Springer <lilyspringer@hotmail.com>
2023-05-30 12:58:09 -0600

    page navigation added

--- src/components/MovieScreen.jsx
+++ src/components/MovieScreen.jsx
@@ -1,14 +1,36 @@
 import MovieCard from "./MovieCard";
 
-const MovieScreen = ({ addMovie, removeMovie, list, page, setPage, movieList }) => {
+const MovieScreen = ({
+  addMovie,
+  removeMovie,
+  list,
+  page,
+  setPage,
+  movieList,
+}) => {
+  const decrement = () => setPage(page--);
+
+  const increment = () => setPage(page++);
+
   const movieDisplay = movieList.map((movie, index) => {
-    return <MovieCard movie={movie} addMovie={addMovie} removeMovie={removeMovie} list={list}/>;
+    return (
+      <MovieCard
+        movie={movie}
+        addMovie={addMovie}
+        removeMovie={removeMovie}
+        list={list}
+      />
+    );
   });
 
   return (
     <div className="page">
       <h1>Lily's Movie Theater</h1>
       <h3>Add a movie to your watchlist!</h3>
+      <div className="button-container">
+        {page !== 1 && <button onClick={decrement}>Previous</button>}
+        <button onClick={increment}>Next</button>
+      </div>
       <div className="movie-container">{movieDisplay}</div>
     </div>
   );
